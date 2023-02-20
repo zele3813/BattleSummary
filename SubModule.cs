6472bd21254ef892f8d72fbdef57d8fb3f11a1ce
Zele D <zeledong3813@gmail.com>
2023-02-20 17:42:31 -0500

    Add a simple ui with ally kill counters

--- SubModule.cs
+++ SubModule.cs
@@ -1,10 +1,13 @@
-﻿using TaleWorlds.Core;
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
 using TaleWorlds.Engine.GauntletUI;
 using TaleWorlds.GauntletUI.Data;
 using TaleWorlds.Library;
 using TaleWorlds.Localization;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.MountAndBlade.View.MissionViews;
+
 
 namespace BattleSummary
 {
@@ -14,6 +17,8 @@ namespace BattleSummary
         protected override void OnSubModuleLoad()
         {
             base.OnSubModuleLoad();
+            Harmony harmony = new Harmony("battle_summary");
+            harmony.PatchAll();
 
         }
 
@@ -22,8 +27,19 @@ namespace BattleSummary
             base.OnMissionBehaviorInitialize(mission);
 
             mission.AddMissionBehavior(new BattleSummaryMissionView());
-
         }
+
+        /*protected override void InitializeGameStarter(Game game, IGameStarter starterObject)
+        {
+            base.InitializeGameStarter(game, starterObject);
+
+            var campaignStarter = starterObject as CampaignGameStarter;
+            if (campaignStarter != null)
+            {
+                campaignStarter.AddBehavior(new )
+            }
+
+        }*/
     }
 
     public class BattleSummaryMissionView : MissionView
@@ -33,12 +49,15 @@ namespace BattleSummary
         GauntletLayer _layer; 
         IGauntletMovie _movie;  
         BattleSummaryVM _dataSource;
+        public BattleSummaryVM DataSource;
+
         public override void OnMissionScreenInitialize()
         {
             base.OnMissionScreenInitialize();
 
             //Set up the new layer
             _dataSource = new BattleSummaryVM(Mission);
+            DataSource = _dataSource;
             _layer = new GauntletLayer(1);
             _movie = _layer.LoadMovie("BattleSummaryHUD", _dataSource);
             MissionScreen.AddLayer(_layer);
@@ -51,7 +70,8 @@ namespace BattleSummary
             MissionScreen.RemoveLayer(_layer);
             _layer = null;
             _movie = null;
-            _dataSource = null;        
+            _dataSource = null;
+            DataSource = null;
         }
 
         //Test method to see if the mission view is responsive. Every time Q is pressed, the count should be incremented.
@@ -62,6 +82,8 @@ namespace BattleSummary
             if (Input.IsKeyReleased(TaleWorlds.InputSystem.InputKey.Q))
             {
                 InformationManager.DisplayMessage(new InformationMessage("Q was pressed."));
+
+                _dataSource.QPressed();
             }
         }
 
@@ -75,7 +97,6 @@ namespace BattleSummary
     public class BattleSummaryVM : ViewModel
     {
         Mission _mission;
-
         public BattleSummaryVM(Mission mission)
         {
             _mission = mission;
@@ -104,6 +125,126 @@ namespace BattleSummary
                     base.OnPropertyChangedWithValue(value, "IsVisible");
                 }
             }
+        }
+
+        //Counter properties for each category of troop type
+        int _infantryKills;
+        [DataSourceProperty]
+        public int InfantryKills
+        {
+            get
+            {
+                return this._infantryKills;
+            }
+            set
+            {
+                if (value != this._infantryKills)
+                {
+                    this._infantryKills = value;
+                    base.OnPropertyChangedWithValue(value, "InfantryKills");
+                }
+            }
+        }
+
+        int _archerKills;
+        [DataSourceProperty]
+        public int ArcherKills
+        {
+            get
+            {
+                return this._archerKills;
+            }
+            set
+            {
+                if (value != this._archerKills)
+                {
+                    this._archerKills = value;
+                    base.OnPropertyChangedWithValue(value, "ArcherKills");
+                }
+            }
+        }
+
+        int _cavalryKills;
+        [DataSourceProperty]
+        public int CavalryKills
+        {
+            get
+            {
+                return this._cavalryKills;
+            }
+            set
+            {
+                if (value != this._cavalryKills)
+                {
+                    this._cavalryKills = value;
+                    base.OnPropertyChangedWithValue(value, "CavalryKills");
+                }
+            }
+        }
+
+        int _cavalryArcherKills;
+        [DataSourceProperty]
+        public int CavalryArcherKills
+        {
+            get
+            {
+                return this._cavalryArcherKills;
+            }
+            set
+            {
+                if (value != this._cavalryArcherKills)
+                {
+                    this._cavalryArcherKills = value;
+                    base.OnPropertyChangedWithValue(value, "CavalryArcherKills");
+                }
+            }
+        }
+
+        //Add to the right category of counter
+        //TODO: Create a separate class to keep track of the counters.
+        public void AddKill(BasicCharacterObject affectorTroop, BasicCharacterObject affectedTroop)
+        {
+            if (affectorTroop.IsInfantry)
+            {
+                InfantryKills += 1;
+                InformationManager.DisplayMessage(new InformationMessage("Infantry kill detected."));
+
+            }else if (affectorTroop.IsRanged && !affectorTroop.IsMounted)
+            {
+                ArcherKills += 1;
+                InformationManager.DisplayMessage(new InformationMessage("Archer kill detected."));
+            }else if (affectorTroop.IsMounted && !affectorTroop.IsRanged)
+            {
+                CavalryKills += 1;
+                InformationManager.DisplayMessage(new InformationMessage("Cavalry kill detected."));
+            }else if (affectorTroop.IsMounted && affectorTroop.IsRanged)
+            {
+                CavalryArcherKills += 1;
+                InformationManager.DisplayMessage(new InformationMessage("Cavalry archer kill detected."));
+            }
+        }
+    }
+
+    //Harmony patch. Postfix. Add the latest kill to the counter whenever a new agent is removed from the battle field. Only tracks the ally kill count as of now.
+    [HarmonyPatch(typeof(Mission), "OnAgentRemoved")]
+    class MissionPatch : HarmonyPatch
+    {
+        static void Postfix(Agent affectedAgent, Agent affectorAgent)
+        {
+            var missionView = Mission.Current.GetMissionBehavior<BattleSummaryMissionView>();
+            if (affectorAgent != null && affectedAgent != null)
+            {
+                if (affectorAgent.Team.IsPlayerAlly)
+                {
+                    //InformationManager.DisplayMessage(new InformationMessage("Player team kill detected."));
+                    missionView.DataSource.AddKill(affectorAgent.Character, affectedAgent.Character);
+                }
+                else
+                {
+                }
+                
+            }
+            
         }
     }
 }
